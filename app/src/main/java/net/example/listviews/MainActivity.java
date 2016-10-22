aad01214802697b34c7d42f519db7b44374e8c7e
Rcorvo <rcu1987@gmail.com>
2016-10-22 17:55:58 +0200

    Ejercicio 3.9 terminado

--- app/src/main/java/net/example/listviews/MainActivity.java
+++ app/src/main/java/net/example/listviews/MainActivity.java
@@ -3,6 +3,7 @@ package net.example.listviews;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,7 +24,7 @@ public class MainActivity extends ListActivity {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, items);
 
         setListAdapter(arrayAdapter);
         selection = (TextView) findViewById(R.id.elemento_seleccionado);
@@ -31,6 +32,17 @@ public class MainActivity extends ListActivity {
 
     @Override
     public void onListItemClick(ListView parent, View v, int position, long id){
-        selection.setText(items[position]);
+        SparseBooleanArray marcados = parent.getCheckedItemPositions();
+        StringBuilder elementos=new StringBuilder();
+        if(marcados!=null){
+            for(int i=0;i<marcados.size();i++){
+                if(marcados.valueAt(i)){
+                    elementos.append(items[marcados.keyAt(i)]);
+                    elementos.append(" + ");
+                }
+            }
+            elementos.delete(elementos.lastIndexOf(" + "), elementos.length()-1);
+        }
+        selection.setText(elementos.toString());
     }
 }
